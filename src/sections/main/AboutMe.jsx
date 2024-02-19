a00c6883aeaa03be074acba1ee7e7392d107bee1
CRXKXWYI49 <134039857+CRXKXWYI49@users.noreply.github.com>
2024-02-18 16:31:39 -0800

    Added animations to About Me Section

--- src/sections/main/AboutMe.jsx
+++ src/sections/main/AboutMe.jsx
@@ -1,6 +1,6 @@
 import { SectionWrapper } from "../../hoc" 
 import { motion } from "framer-motion"
-import { textVariant } from "../../utils/motion"
+import { textVariant, zoomIn } from "../../utils/motion"
 import { aboutMe } from "../../constants/main/about-me-section"
 import { v6engine } from "../../assets"
 
@@ -12,12 +12,17 @@ const AboutMe = () => {
         <div className="font-bold font-poppins text-5xl py-5 mx-auto lg:mx-0">About Me</div>
         <div className="flex flex-col gap-5 lg:flex-row justify-between">
 
-          <motion.div className="lg:w-1/2"
-                      variants={textVariant(0.5)}>
-            <p className="font-dark font-poppins text-4xl">{aboutMe[0].school}</p>
-            <p className="font-thin text-3xl py-5"> {aboutMe[0].bio}</p>
-
+          <div className="flex flex-col lg:w-1/2">
+          <motion.div variants={zoomIn(0.2, 0.5)}
+                      className="mx-auto lg:mx-0">
+            <div className="font-dark font-poppins text-4xl">
+              {aboutMe[0].school} 
+            </div>
           </motion.div>
+          <motion.div variants={textVariant(0.5)}>
+            <p className="font-thin text-3xl py-5"> {aboutMe[0].bio}</p>
+          </motion.div>
+        </div>
 
           <img src={ v6engine }
                 className="rounded-2xl lg:w-1/2"
